807b16aee3e85bd416101da3891f4c8a2a80d5da
Zulqarnain <zijaz1055@gmail.com>
2025-05-26 16:57:58 +0500

    temporary

--- Registration module/registration/src/Login2.jsx
+++ Registration module/registration/src/Login2.jsx
@@ -42,14 +42,14 @@ function Loginn() {
 
     if (!validate()) return;
     try {
-      const response =await axios.post("http://127.0.0.1:8000/api/login/  ",{
+      const response =await axios.post("http://127.0.0.1:8000/api/login/",{
         username,
         password
 
       });
       console.log("response",response.data);
       navigate("/homepage");
-      // alert("login successfuly");
+      alert("login successfuly");
       
     } catch (error) {
       console.log("error occoured",error);
