46a2f551bb6e3975eaa99210ce37bb7c9a90cf9d
usama <usamabutt21019@gmail.com>
2025-06-11 08:05:09 +0500

    recomendation modal updated

--- Registration module/registration/src/components/Navbar/Dashnav.jsx
+++ Registration module/registration/src/components/Navbar/Dashnav.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import PersonalProfile from '../../pages/account';
 import axiosInstance from "../../api/axiosInstance";
